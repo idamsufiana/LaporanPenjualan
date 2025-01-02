a64ba85412019e92dd3c1dbf86c695894b72fff2
ida.sufiana <ida.sufiana@consultant.ioh.co.id>
2025-01-02 10:10:04 +0700

    update latest

--- src/main/java/com/aegis/laporan/penjualan/config/WebConfig.java
+++ src/main/java/com/aegis/laporan/penjualan/config/WebConfig.java
@@ -1,4 +1,20 @@
 package com.aegis.laporan.penjualan.config;
 
-public class WebConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Autowired
+    private TokenInterceptor tokenInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(tokenInterceptor)
+                .addPathPatterns("/crud/**")
+                .order(Ordered.HIGHEST_PRECEDENCE);
+    }
 }
